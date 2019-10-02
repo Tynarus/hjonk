3d437784b7eb218d5db5b1f39684c9b86346f605
Tynarus <admin@tynarus.com>
2019-10-02 18:35:02 -0500

    Adding mobile-friendly navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +17,8 @@ import { NotFoundModule } from './view/not-found/not-found.module';
         BrowserAnimationsModule,
         MatToolbarModule,
         MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
         NotFoundModule
     ],
     providers: [],
