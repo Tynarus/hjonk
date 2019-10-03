a5f8dd1d1498731105549e6839f22d84c7d30cb3
Tynarus <admin@tynarus.com>
2019-10-03 11:27:07 -0500

    Adding Contact module and component and fixing whitespace in the about section.

--- src/app/view/contact/contact-routing.module.ts
+++ src/app/view/contact/contact-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: AboutComponent
+        component: ContactComponent
     }
 ];
 
@@ -13,5 +13,5 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class AboutRoutingModule {
+export class ContactRoutingModule {
 }
